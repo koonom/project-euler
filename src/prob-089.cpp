df0addb02f5881ffbcb34abdf2e3ed0f2fa25a69
Ruey-Cheng Chen <rueycheng@gmail.com>
2011-07-22 00:43:46 +0800

    Need more info on the numerals

--- src/prob-089.cpp
+++ src/prob-089.cpp
@@ -51,25 +51,27 @@ bool intToRoman(int n, int lb, int ub, std::string& result) {
 
 	    if (n >= values[i]) {
 		std::string buf(q, letters[i]);
-		std::cerr << head << " [1] " << result << '|' << buf << " " << r << std::endl;
+		std::cerr << head << " 1: " << result << '|' << buf << " " << r << std::endl;
 		if (!intToRoman(r, lb, i, buf)) continue;
 		if (bestSoFar.empty() || bestSoFar.size() > buf.size()) {
 		    bestSoFar = buf;
-		    std::cerr << head << " 1: " << bestSoFar << std::endl;
+		    std::cerr << head << " [1] " << bestSoFar << std::endl;
 		}
 	    }
 
 	    // Case 2: q...q + r q
-	    r -= values[i]; // < 0
+	    if (r > 0) {
+		r -= values[i]; // < 0
 
-	    std::string buf(q, letters[i]), buf2(1, letters[i]);
-	    std::cerr << head << " [2] " << result << '|' << buf << ' ' << r << ' ' << buf2 << std::endl;
-	    if (!intToRoman(r, lb, i % 2 + i - 2, buf2)) continue; // abuse, note 2
-	    buf += buf2;
+		std::string buf(q, letters[i]), buf2(1, letters[i]);
+		std::cerr << head << " 2: " << result << '|' << buf << ' ' << r << ' ' << buf2 << std::endl;
+		if (!intToRoman(r, lb, i % 2 + i - 2, buf2)) continue; // abuse, note 2
+		buf += buf2;
 
-	    if (bestSoFar.empty() || bestSoFar.size() > buf.size()) {
-		bestSoFar = buf;
-		std::cerr << head << " 2: " << bestSoFar << std::endl;
+		if (bestSoFar.empty() || bestSoFar.size() > buf.size()) {
+		    bestSoFar = buf;
+		    std::cerr << head << " [2] " << bestSoFar << std::endl;
+		}
 	    }
 	}
 
@@ -77,40 +79,70 @@ bool intToRoman(int n, int lb, int ub, std::string& result) {
     }
     else {
 	int nn = -n;
-	std::string buf;
 
 	if (ub != 0 && ub != 2 && ub != 4) return false;
 
-	if (nn < values[ub]) {
-	    int q = 1;
-	    int r = values[ub] - nn; // > 0
+	int q = nn / values[ub], r = nn % values[ub];
 
-	    std::string buf(1, letters[ub]);
-
-	    std::string buf2;
-	    std::cerr << head << " [3] " << buf << '|' << result << " " << r << std::endl;
-	    if (!intToRoman(r, lb, ub + 1, buf2)) return false;
-
-	    std::cerr << head << " 3: " << buf << "-" << buf2 << std::endl;
-
-	    bestSoFar = buf + buf2;
-	    result.insert(0, buf);
-	    result.append(buf2);
+	if (r > 0) {
+	    q += 1;
+	    r -= values[ub];
 	}
-	else {
-	    int q = nn / values[ub];
-	    int r = nn % values[ub];
 
-	    buf.append(q, letters[ub]);
-	    int p = ub - 2 + ub % 2;
-	    std::cerr << head << " [4] " << -r << ' ' << buf << '|' << result << std::endl;
-	    if (!intToRoman(-r, lb, p, buf)) return false;
+	// Case 3:  qqq ??? rrr
+	std::string buf, prefix(q, letters[ub]);
+	std::cerr << head << " 3: " << prefix << " (" << result << ") " << r << std::endl;
+	if (!intToRoman(-r, lb, ub + 1, buf)) return false; // -r > 0
 
-	    bestSoFar = buf;
-	    std::cerr << head << " 4: " << bestSoFar << std::endl;
+	bestSoFar = prefix + buf;
+	result.insert(0, prefix);
+	result.append(buf);
+	//--------------------------------------------------
+	// if (nn < values[ub]) {
+	//     int q = 1;
+	//     int r = values[ub] - nn; // > 0
+	//-------------------------------------------------- 
 
-	    result.insert(0, bestSoFar);
-	}
+	//--------------------------------------------------
+	//     std::string buf(1, letters[ub]);
+	//-------------------------------------------------- 
+
+	//--------------------------------------------------
+	//     std::string buf2;
+	//     std::cerr << head << " 3: " << buf << '|' << result << " " << r << std::endl;
+	//     if (!intToRoman(r, lb, ub + 1, buf2)) return false;
+	//-------------------------------------------------- 
+
+	//--------------------------------------------------
+	//     std::cerr << head << " [3] " << buf << " ? " << buf2 << std::endl;
+	//-------------------------------------------------- 
+
+	//--------------------------------------------------
+	//     bestSoFar = buf + buf2;
+	//     result.insert(0, buf);
+	//     result.append(buf2);
+	// }
+	// else {
+	//     int q = nn / values[ub];
+	//     int r = nn % values[ub];
+	//-------------------------------------------------- 
+
+	//--------------------------------------------------
+	//     buf.append(q, letters[ub]);
+	//     int p = ub - 2 + ub % 2;
+	//     std::cerr << head << " 4: " << -r << ' ' << buf << '|' << result << std::endl;
+	//     if (!intToRoman(-r, lb, p, buf)) return false;
+	//-------------------------------------------------- 
+
+	//--------------------------------------------------
+	//     bestSoFar = buf;
+	//     std::cerr << head << " [4] " << bestSoFar << std::endl;
+	//-------------------------------------------------- 
+
+	//--------------------------------------------------
+	//     result.insert(0, bestSoFar);
+	// }
+	//-------------------------------------------------- 
 
     }
 
