8bd73b3b7ec0868232fd505ae7fbc7e5b49c7348
Ruey-Cheng Chen <rueycheng@gmail.com>
2011-07-24 23:19:53 +0800

    100!

--- src/prob-100.cpp
+++ src/prob-100.cpp
@@ -1,27 +1,32 @@
+#include <cmath>
 #include <iostream>
 #include <gmpxx.h>
 
-int main() {
-    mpz_class m, numberOfBlueDiscs;
-    mpz_ui_pow_ui(m.get_mpz_t(), 10, 12);
+#include "ContinuedFraction.hpp"
 
-    while (true) {
-	bool found = false;
-	mpz_class ub = m * 71 / 100, dest = m * (m - 1);
-	std::cout << m << std::endl;
-	for (mpz_class n = m * 7 / 10; n <= ub; ++n) {
-	    std::cout << n << ' ' << ub << std::endl;
-	    if (2 * n * (n - 1) == dest) {
-		numberOfBlueDiscs = n;
-		found = true;
+// NOTE: I got stuck for two days and try bruteforcing my way out.
+//
+// Courtesy of http://mukeshiiitm.wordpress.com/2011/02/11/project-euler-problem-100/
+int main() {
+    mpz_class N;
+    mpz_ui_pow_ui(N.get_mpz_t(), 10, 12);
+
+    ContinuedFraction fraction(8);
+
+    for (int n = 1; ; ++n) {
+	mpq_class c = fraction.getConvergent(n);
+
+	mpz_class x2, r2;
+	mpz_pow_ui(x2.get_mpz_t(), c.get_num().get_mpz_t(), 2);
+	mpz_pow_ui(r2.get_mpz_t(), c.get_den().get_mpz_t(), 2);
+
+	if (x2 - 8 * r2 == 1) {
+	    mpz_class result = 2 * c.get_den() + (c.get_num() + 1) / 2;
+	    if (result > N) {
+		std::cout << result - c.get_den() << "\n";
 		break;
 	    }
 	}
-
-	if (found) break;
-	++m;
     }
-
-    std::cout << numberOfBlueDiscs << "\n";
     return 0;
 }
