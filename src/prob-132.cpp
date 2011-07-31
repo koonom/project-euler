532659e4703912e8b868500520a025580a1d5a3c
Ruey-Cheng Chen <rueycheng@gmail.com>
2011-08-01 00:53:36 +0800

    Kind of like this series of repunit problems

---
+++ src/prob-132.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// Shameless copy from Problems 129 and 130
+const int N = 1000000, M = 1000000000;
+bool composite[N + 1];
+
+// NOTE: Runs in 30 seconds.  Merely okay.
+int main() {
+    for (int n = 2; n <= N / 2; ++n)
+	if (!composite[n])
+	    for (int m = n + n; m <= N; m += n) composite[m] = true;
+
+    int sum = 0;
+    for (int n = 3, i = 1; i <= 40 && n <= N; n += 2) {
+	if (n % 5 == 0) continue;
+	if (composite[n]) continue;
+
+	int j = 1, m = 1;
+	while (j <= M && (m %= n) > 0) {
+	    m *= 10;
+	    ++m;
+	    ++j;
+	}
+
+	if (j > M) continue;
+	if (M % j == 0) {
+	    sum += n;
+	    ++i;
+	}
+    }
+
+    std::cout << sum << "\n";
+    return 0;
+}
